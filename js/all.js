21819a6982b728a84230fcd79ca2d646885396ca
hobbyling <g99949@hotmail.com>
2016-10-17 18:56:38 +0800

    add js, css

---
+++ js/all.js
@@ -0,0 +1,6 @@
+function add(){
+	var num1 = parseInt(document.getElementById('num1').value);
+	var num2 = parseInt(document.getElementById('num2').value);
+	var num=num1+num2;
+	alert("總和：" + num);
+}
